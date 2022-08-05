a476f281e22d90fce4874056984dc094bcb2110d
unknown <munna.cse.pust@gmail.com>
2022-08-05 17:06:50 +0600

    drag and drop completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [firstState, setFirstState] = useState([
-    { id: 1, title: "item 1" },
-    { id: 2, title: "item 2" },
-    { id: 3, title: "item 3" },
-    { id: 4, title: "item 4" },
-    { id: 5, title: "item 5" },
-  ]);
+  const [inputText, setInputText] = useState("");
+  const [selectParent, setSelectParent] = useState("");
+  const [countTodo, setcountTodo] = useState(5);
 
-  const [secondtState, setSecondState] = useState([]);
+  const [mainToDoState, setMainToDoState] = useState({
+    serverSide: [],
+    clientSide: [],
+  });
 
-  // console.log("firstState : ", firstState);
-  console.log("secondtState : ", secondtState);
+  const [progressToDoState, setProgressToDoState] = useState({
+    serverSide: [],
+    clientSide: [],
+  });
+
+  const [finalToDoState, setFinalToDoState] = useState({
+    serverSide: [],
+    clientSide: [],
+  });
+
+  // add to do handler
+  const addToDoHandler = (e) => {
+    e.preventDefault();
+    console.log("inputText : ", inputText);
+
+    setcountTodo((prev) => prev + 1);
+
+    setMainToDoState((prev) => {
+      return {
+        ...mainToDoState,
+        clientSide: [
+          ...mainToDoState.clientSide,
+          { id: countTodo + 1, title: inputText },
+        ],
+        serverSide: [
+          ...mainToDoState.serverSide,
+          { id: countTodo + 1, title: inputText },
+        ],
+      };
+    });
+
+    // clear input filed
+    setInputText("");
+  };
+
+  console.log("mainToDoState : ", mainToDoState);
+  console.log("progressToDoState : ", progressToDoState);
+
+  console.log("finalToDoState : ", finalToDoState);
 
   // drag start
   const dragStartHandler = (e) => {
@@ -23,17 +59,10 @@ function App() {
     e.dataTransfer.dropEffect = "move";
     e.dataTransfer.effectAllowed = "move";
 
-    if (e.target.parentElement.id == "ul-2") {
-      let removeItem = secondtState.filter((item) => item.id != e.target.id);
-
-      console.log("removeItem :", removeItem);
-
-      setSecondState((prev) => {
-        return [...prev.filter((item) => item.id != e.target.id)];
-      });
-    }
-
     e.dataTransfer.setData("text/html", e.target.id);
+
+    // select parent container
+    setSelectParent(e.target.parentElement.id);
   };
 
   // drop handler
@@ -42,17 +71,63 @@ function App() {
 
     let data = e.dataTransfer.getData("text/html");
 
-    let dragedItem = firstState.find((item) => item.id == data);
+    e.target.appendChild(document.getElementById(data));
 
-    let findItem2 = secondtState.find((item) => item.id == data);
+    let dragedItem;
 
-    if ((num = 2 && !findItem2)) {
-      // setSecondState((prev) => {
-      //   return [...prev, dragedItem];
-      // });
+    // select data comes from where
+    if (selectParent == "c1") {
+      dragedItem = mainToDoState.clientSide.find((item) => item.id == data);
+    } else if (selectParent == "c2") {
+      dragedItem = progressToDoState.clientSide.find((item) => item.id == data);
+    } else {
+      dragedItem = finalToDoState.clientSide.find((item) => item.id == data);
     }
 
-    e.target.appendChild(document.getElementById(data));
+    if (e.target.id == "c1" && selectParent != e.target.id) {
+      console.log("in drop dragedItem 1 ::", dragedItem);
+
+      setMainToDoState((prev) => {
+        return { ...prev, clientSide: [...prev.clientSide, dragedItem] };
+      });
+    } else if (e.target.id == "c2" && selectParent != e.target.id) {
+      console.log("in drop dragedItem 2 ::", dragedItem);
+
+      setProgressToDoState((prev) => {
+        return { ...prev, clientSide: [...prev.clientSide, dragedItem] };
+      });
+    } else {
+      setFinalToDoState((prev) => {
+        return { ...prev, clientSide: [...prev.clientSide, dragedItem] };
+      });
+    }
+
+    // remove item from list when drag start
+    if (selectParent == "c1" && selectParent != e.target.id) {
+      let othersItem = mainToDoState.clientSide.filter(
+        (item) => item.id != data
+      );
+
+      setMainToDoState((prev) => {
+        return { ...prev, clientSide: othersItem };
+      });
+    } else if (selectParent == "c2" && selectParent != e.target.id) {
+      let othersItem = progressToDoState.clientSide.filter(
+        (item) => item.id != data
+      );
+
+      setProgressToDoState((prev) => {
+        return { ...prev, clientSide: othersItem };
+      });
+    } else {
+      let othersItem = finalToDoState.clientSide.filter(
+        (item) => item.id != data
+      );
+
+      setFinalToDoState((prev) => {
+        return { ...prev, clientSide: othersItem };
+      });
+    }
 
     return false;
   };
@@ -80,6 +155,28 @@ function App() {
     e.target.style.backgroundColor = "blueviolet";
   };
 
+  useEffect(() => {
+    setMainToDoState((prev) => {
+      return {
+        ...mainToDoState,
+        serverSide: [
+          { id: 1, title: "item 1" },
+          { id: 2, title: "item 2" },
+          { id: 3, title: "item 3" },
+          { id: 4, title: "item 4" },
+          { id: 5, title: "item 5" },
+        ],
+        clientSide: [
+          { id: 1, title: "item 1" },
+          { id: 2, title: "item 2" },
+          { id: 3, title: "item 3" },
+          { id: 4, title: "item 4" },
+          { id: 5, title: "item 5" },
+        ],
+      };
+    });
+  }, []);
+
   return (
     <div
       style={{
@@ -88,14 +185,29 @@ function App() {
     >
       <h2>React Drag and Drop</h2>
 
+      <div style={{ width: "60%", margin: "auto", padding: "20px" }}>
+        <input
+          type="text"
+          style={{ padding: "10px" }}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <input
+          style={{ padding: "10px" }}
+          type="submit"
+          value="Submit"
+          onClick={(e) => addToDoHandler(e)}
+        />
+      </div>
+
       <div className="container">
         <div className="child_container">
           <ul
-            id="ul-1"
+            id="c1"
             onDrop={(e) => dropHandler(e, 1)}
             onDragOver={(e) => dragOverHandler(e)}
           >
-            {firstState.map((item) => (
+            {mainToDoState.serverSide.map((item) => (
               <li
                 id={item.id}
                 key={item.id}
@@ -112,7 +224,14 @@ function App() {
         </div>
         <div className="child_container">
           <ul
-            id="ul-2"
+            id="c2"
+            onDrop={(e) => dropHandler(e, 2)}
+            onDragOver={(e) => dragOverHandler(e)}
+          ></ul>
+        </div>
+        <div className="child_container">
+          <ul
+            id="c3"
             onDrop={(e) => dropHandler(e, 2)}
             onDragOver={(e) => dragOverHandler(e)}
           ></ul>
